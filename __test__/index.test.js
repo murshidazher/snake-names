50444eefa96b715c7d5527889ef14558fec3c88b
murshidazher <murshidazher@yahoo.com>
2020-12-09 21:56:25 +0530

    feat: update the test files

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -6,8 +6,8 @@ const allSnake = snakeNames.all;
 
 describe('snake names', function() {
   describe('all', function() {
-    it('should return an object containing all 200 snake names', function() {
-      expect(Object.keys(allSnake).length).toEqual(200);
+    it('should return an object containing all 555 snake names', function() {
+      expect(Object.keys(allSnake).length).toEqual(555);
     });
   });
 
